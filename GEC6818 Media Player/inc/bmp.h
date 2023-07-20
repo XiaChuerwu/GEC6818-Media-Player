ac86ea091165575e348bd0ef892f3704be0bbca1
XiaChuerwu <93534259+XiaChuerwu@users.noreply.github.com>
2023-07-20 16:17:31 +0800

    Delete GEC6818 Media Player directory

--- GEC6818 Media Player/inc/bmp.h
+++
@@ -1,6 +0,0 @@
-#ifndef __BMP_H__
-#define __BMP_H__
-
-void display_bmp(const char * pathname, int x0, int y0);
-
-#endif
--- GEC6818 Media Player/inc/jpeg.h
+++
@@ -1,6 +0,0 @@
-#ifndef __JPEG_H__
-#define __JPEG_H__
-
-void display_jpeg(const char * pathname, int x0, int y0);
-
-#endif
